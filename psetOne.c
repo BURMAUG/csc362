81614b2877dffb8e81c92ed531199b516c939789
Deejai <burmaud1@gmail.com>
2020-08-19 21:12:06 -0400

    First commit

---
+++ psetOne.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+
+   char x; 
+   int counter = 0, skip = 5;
+  
+   //get the files 
+   FILE *inputFile = fopen("infile.txt", "r");
+   FILE *outFile = fopen("outFile.txt", "w");
+   //get the number to skip from the user
+   //printf("Enter the skip number: %d ", skip);
+   //scanf("%d", &skip);
+   //
+
+      while ((x = getc(inputFile)) != -1)
+    {
+
+        if (counter < skip)
+        {
+            putc(x, outFile);
+            counter++;
+        }
+        else
+        {
+
+            if (ispunct(x) || isspace(x) || x == '\t')
+            {
+                putc(x, outFile);
+            };
+
+            counter = 0;
+        }
+    };
+    fclose(outFile);
+    fclose(inputFile);
+}
