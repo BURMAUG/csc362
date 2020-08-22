db9bcb5ba29856ffb7b4722edfc958af56d4d076
Deejai <burmaud1@gmail.com>
2020-08-22 14:11:52 -0400

    space update

--- psetOne.c
+++ psetOne.c
@@ -6,13 +6,13 @@ int main()
     char x;
     int counter = 0, skip = 5;
 
+    char c[2] = "s";
+    char *p = &c[2];
+    printf("%s", p);
+
     //get the files
     FILE *inputFile = fopen("infile.txt", "r");
     FILE *outFile = fopen("outFile.txt", "w");
-    //get the number to skip from the user
-    //printf("Enter the skip number: %d ", skip);
-    //scanf("%d", &skip);
-    //
 
     while ((x = getc(inputFile)) != -1)
     {
@@ -24,14 +24,12 @@ int main()
         }
         else
         {
-
             if (ispunct(x) || isspace(x) || x == '\n')
             {
                 putc(x, outFile);
             };
-
             counter = 0;
-        }
+        };
     };
     fclose(outFile);
     fclose(inputFile);
